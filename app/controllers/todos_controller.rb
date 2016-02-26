854b302ad143b7b48ed1a52d6af5e1d3d85f9fb2
Patrick Clark <patclark1958@gmail.com>
2016-02-26 11:15:26 -0600

    Starting point for today. I really want to have this project all the way through CRUD. Eager to get something done. 1. Need to fix post-redirect-get for the delete todo. 2. Link pages together where I have not done that yet. 3. Change todos_new to set value of "false" to a new todo. Currently not doing anything on that.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,32 @@
 
 
-#This is for a post - redirect - get. 
-# MyApp.get "todos/todos_create" do
-#   @todos = Todo.find_by_id(session[:user_id])
+# This is for a post - redirect - get. 
 
-#   erb  :"/todos/todos_home"
-# end
+#Shows the edit form
+MyApp.get "/todos/todos_edit/:id" do
+  @todos = Todo.find_by_id(params[:id])
+
+  erb :"/todos/todos_edit"
+end
+
+#Processes the edit form
+MyApp.post "/todos/todos_update/:id" do
+  @todos = Todo.find_by_id(params[:id])
+  @todos.title = (params[:title])
+  @todos.description = (params[:description]) 
 
 
+  @todos.completed = !(params[:completed].nil?)
+
+  # if params[:completed].first == "on"
+  #   @todos.completed = true
+  # else
+  #   @todos.completed = false
+  # end
+  @todos.save
+
+  erb :"todos/todos_update"
+end
 
 #Home page for the To Do list program. Will display the To Do list.
 #Each todo will be An Array object. The .erb page will have to use an each do loop to iterate through each object and display the title-description-completed. Not sure how to use the id column quite yet. Do I need to capture a session id to make a new to do on this list?  
@@ -30,6 +49,7 @@ MyApp.get "/todos/todos_new" do
     redirect "/logins/login_new"
   end
 end
+
 #This controller action will process the new todo form to create a record of a new todo.
 MyApp.post "/todos/todos_create" do
   @todo = Todo.new
@@ -42,12 +62,13 @@ MyApp.post "/todos/todos_create" do
   erb :"/todos/todos_create" #build a success page with link to home and link to add another todo and link to logout
 end
 
-#need an edit page to display the edit form, and an update page to process the edit form.
 
-MyApp.post "/todos/todo_delete" do
+#I am asserting that "delete" is the ultimate edit, also asserting the users would prefer to delete from the edit page rather than have to go to another page to do that. So I have added a delete button to the edit page. 
+
+MyApp.post "/todos/todos_delete/:id" do
   @todo = Todo.find_by_id(session[:user_id])
   @todo.delete
 
-
-  erb :"todo_deleted"#make a success page 
+  @todos = Todo.all
+  redirect "/todos/todo_home" 
 end
